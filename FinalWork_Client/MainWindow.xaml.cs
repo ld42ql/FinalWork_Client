63bd4edb286518fb1ac6a4b2b8a2b9af2da837e9
Vladimir baleev <ld42ql@hotmail.com>
2018-01-27 11:51:57 +0300

    Добавил выбор работника по его ID

--- FinalWork_Client/MainWindow.xaml.cs
+++ FinalWork_Client/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using Newtonsoft.Json.Linq;
+using System.Collections.ObjectModel;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,6 +14,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Http;
+using System.Data;
+using FinalWork_Client.Models;
 
 namespace FinalWork_Client
 {
@@ -20,9 +25,22 @@ namespace FinalWork_Client
     /// </summary>
     public partial class MainWindow : Window
     {
+        WorkingAPI test = new WorkingAPI();
+
         public MainWindow()
         {
             InitializeComponent();
+            
+            listView.MouseDoubleClick +=
+                delegate
+                {
+                    new EditWindow(listView.SelectedItem as Employee).ShowDialog();
+                };
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            listView.DataContext = test.ViewList();
         }
     }
 }
---
+++ FinalWork_Client/Models/WorkingAPI.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Windows;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace FinalWork_Client.Models
+{
+    
+
+    class WorkingAPI
+    {
+        WebClient webClient = new WebClient();
+
+        /// <summary>
+        /// Создаем адрес, с кодировкой UTF8
+        /// </summary>
+        /// <param name="urlMetod">Имя метода из API</param>
+        /// <returns></returns>
+        private string ConnectionUrl(string urlMetod)
+        {
+            webClient.Encoding = System.Text.Encoding.UTF8;
+            return $"http://localhost:62975/{urlMetod}";
+        }
+
+        /// <summary>
+        /// Показать всю таблицу
+        /// </summary>
+        /// <returns></returns>
+        public ObservableCollection<Employee> ViewList()
+        {
+            var temp = webClient.DownloadString(ConnectionUrl("getList/"));
+
+            return JsonConvert.DeserializeObject<ObservableCollection<Employee>>(temp);
+        }
+
+        /// <summary>
+        /// Показать работника
+        /// </summary>
+        /// <param name="id">Номер работника</param>
+        /// <returns></returns>
+        public Employee ViewEmployee(int id)
+        {
+            var temp = webClient.DownloadString($"{ConnectionUrl("getList/")}{id}");
+            return JsonConvert.DeserializeObject<Employee>(temp);
+        }
+    }
+}
